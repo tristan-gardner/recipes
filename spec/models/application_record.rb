155a38fdd6158f2d8e6fa98f4ee46a323f9e0e67
Kibisis <jdonahue@colgate.edu>
2019-03-30 13:12:35 -0400

    Rspec files created, not filled yet.

---
+++ spec/models/application_record.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# Rspec.describe Application_Record, type: :model do
+#   describe "check attributes and methods" do
+#     it "should be able to create an Application_Record object which has the correct methods" do
+#       a_p = Application_Record.create!()
+#
+#     end
+#   end
+#
+# end
