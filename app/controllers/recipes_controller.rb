7e13d1d805f9bfa7b445e17e3b5e58b3bc3f4f08
Tristan <trgardner123@gmail.com>
2019-03-31 17:58:17 -0400

    index, show and create conrtoller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,34 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipes.all
+  end
+
+  def show
+    id = params[:id]
+    begin
+      @recipe = Recipe.find(id)
+    rescue ActiveRecord::RecordNotFound
+      redirect_to products_path
+      return
+    end
+  end
+
+  def create
+    begin
+      r = Recipes.new(create_update_params)
+      r.save
+      flash[:notice] = "Recipe successfully added"
+      redirect_to(recipes_path)
+      return
+    rescue
+      flash[:warning] = "An error occurred, recipe not added"
+      redirect_to(new_recipe_path)
+      return
+    end
+  end
+
+  private
+  def create_update_params
+    params.require(:recipe).permit(:name,:directions,:cuisine,:calories)
+  end
 end
