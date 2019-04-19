115555df588fb4d8e9f8898f5f4f4ed2e2a0c60e
Cristian Saguil <cristian.saguil@gmail.com>
2019-04-19 14:24:17 -0400

    added view more button to show all reviews for a recipe

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,10 @@ class ReviewsController < ApplicationController
     return
   end
 
+  def index
+    @reviews = Review.where("recipe_id = #{params[:recipe_id]}")
+  end
+
   private
   def create_update_params
     params.require(:review).permit(:description, :rating, :user_id, :recipe_id)
