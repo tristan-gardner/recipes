0d52ea915e6cf9d643e444e428132407af992d0b
Cristian Saguil <cristian.saguil@gmail.com>
2019-04-29 00:40:43 -0400

    fixed problem is models spec where user was not being saved

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -6,12 +6,12 @@ SimpleCov.start
 
 RSpec.describe Recipe, type: :model do
   before(:each) do
-    user = User.where("username = user")
+    @user = User.create!(username: "a", email: "a@a.com", password: "password")
   end
   describe "check attributes and methods" do
     it "should be able to create an Recipe object which has the correct methods" do
       r = Recipe.new(name: "Bacon Burger", directions: "Blended up burger with bacon bits sprinkled on top",calories: 2000, cuisine: "Druggie Lunch")
-      r.user = user
+      r.user = @user
       r.save!
       expect(r).to respond_to :name
       expect(r).to respond_to :directions
@@ -21,11 +21,11 @@ RSpec.describe Recipe, type: :model do
     end
     it "should fail to be created when a name is not supplied" do
       expect {
-        r = Recipe.create!(directions: "Blended up burger with bacon bits sprinkled on top")}.to raise_error(ActiveRecord::NotNullViolation)
+        r = Recipe.create!(directions: "Blended up burger with bacon bits sprinkled on top")}.to raise_error(ActiveRecord::RecordInvalid)
     end
     it "should be created when directions are not supplied but a name is" do
       r = Recipe.new(name: "Bacon Burger", directions: "Blended up burger with bacon bits sprinkled on top")
-      r.user = user
+      r.user = @user
       r.save
       expect(r).not_to eq(nil)
     end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,12 +6,12 @@ SimpleCov.start
 
 RSpec.describe Review, type: :model do
   before(:each) do
-    user = User.where("username = user")
+    @user = User.create!(username: "a", email: "a@a.com", password: "password")
   end
   describe "check attributes and methods" do
     it "should be able to create an Review object" do
       a_p = Review.new(rating: 1, description: "This is horrifying")
-      a_p.user = user
+      a_p.user = @user
       expect(a_p).to respond_to :rating
       expect(a_p).to respond_to :description
     end
@@ -20,13 +20,13 @@ RSpec.describe Review, type: :model do
     end
     it "should to be created when the rating is greater than 5 (#FeatureNotABug)" do
       a_p = Review.new(rating: 10, description: "Wow amazing")
-      a_p.user = user
+      a_p.user = @user
       a_p.save
       expect(a_p).not_to eq(nil)
     end
     it "should to be created when the rating is less than 0 (#FeatureNotABug)" do
       a_p = Review.new(rating: -1, description: "Wow amazing")
-      a_p.user = user
+      a_p.user = @user
       a_p.save
       expect(a_p.description).not_to eq(nil)
     end
