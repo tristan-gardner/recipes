c5a99104a18a0d4d5aef1d566d2b2d9d3d8dcf56
Tristan <trgardner123@gmail.com>
2019-04-29 19:28:27 -0400

    filtering by ingredient(singular) now works but the tests are broken

--- spec/features/index_request_spec.rb
+++ spec/features/index_request_spec.rb
@@ -12,10 +12,18 @@ RSpec.describe "index page", type: :feature do
 
   before :each do
     user = User.create!(:username => "user1", :email => "user1@user.com", :password => "password")
-    Recipe.create!(name: "Pizza", directions: "make a pizza", cuisine: "Italian", calories: 800, user_id: user.id)
-    Recipe.create!(name: "Lasagna", directions: "Kind of like layered pasta", cuisine: "italian", calories: 1000, user_id: user.id)
-    Recipe.create!(name: "Chicken Adobo", directions: "Saguil's famous dish", cuisine: "Filipino", calories: 400, user_id: user.id)
-    Recipe.create!(name: "Latkas", directions: "Round hash browns", cuisine: "Jewish", calories: 200, user_id: user.id)
+    r1 = Recipe.create!(name: "Pizza", directions: "make a pizza", cuisine: "Italian", calories: 800, user_id: user.id)
+    r2 = Recipe.create!(name: "Lasagna", directions: "Kind of like layered pasta", cuisine: "italian", calories: 1000, user_id: user.id)
+    r3 = Recipe.create!(name: "Chicken Adobo", directions: "Saguil's famous dish", cuisine: "Filipino", calories: 400, user_id: user.id)
+    r4 = Recipe.create!(name: "Latkas", directions: "Round hash browns", cuisine: "Jewish", calories: 200, user_id: user.id)
+    # ts = Ingredient.create!(name: "Tomato Sauce")
+    # chic = Ingredient.create!(name: "chicken")
+    # po = Ingredient.create!(name: "Potatos")
+    #
+    # r1.ingredients << ts
+    # r2.ingredients << ts
+    # r3.ingredients << chic
+    # r4.ingredients << po
 
     visit "/recipes"
   end
@@ -51,6 +59,28 @@ RSpec.describe "index page", type: :feature do
     page.all(".title").each { |x| names << x.text }
     expect(names.length).to eq(2)
     fill_in "Cuisine Type", :with => "Filipino"
+    click_button "Filter"
+    names = []
+    page.all(".title").each { |x| names << x.text }
+    expect(names.length).to eq(1)
+    click_link "Clear filter"
+    names = []
+    page.all(".title").each { |x| names << x.text }
+    expect(names.length).to eq(4)
+  end
+
+  it "should do the correct filtering when filtering by Ingredient" do
+    fill_in "Ingredient", :with => "Tomato Sauce"
+    click_button "Filter"
+    visit "/recipes"
+    names = []
+    page.all(".title").each { |x| names << x.text }
+    expect(names.length).to eq(2)
+    visit "/recipes"
+    names = []
+    page.all(".title").each { |x| names << x.text }
+    expect(names.length).to eq(2)
+    fill_in "Ingredient", :with => "Potato"
     click_button "Filter"
     names = []
     page.all(".title").each { |x| names << x.text }
