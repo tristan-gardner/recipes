4651f2f01461f8e91cd9d65d591ae47fe243d3fb
Cristian Saguil <cristian.saguil@gmail.com>
2019-04-29 15:34:45 -0400

    added review form to show page for recipe

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,30 @@ class ReviewsController < ApplicationController
     @reviews = Review.where("recipe_id = #{params[:recipe_id]}")
   end
 
+  def add_review
+    begin
+      recipe = Recipe.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "No such recipe"
+      redirect_to recipes_path and return
+    end
+    puts params
+    review = recipe.reviews.create(create_update_params)
+    respond_to do |format|
+      format.html do
+        # existing code that renders or redirects.
+        flash[:notice] = "You reviewed #{recipe.name}"
+        redirect_to recipe_path(recipe) and return
+      end
+      format.json do
+        # new code that handles responding to an AJAX call with some
+        # JSON.
+        puts "kjfhlaskdjfhlskdjh"
+        render json: { review: review  }, :status => :ok
+      end
+    end
+  end
+
   private
   def create_update_params
     params.require(:review).permit(:description, :rating, :user_id, :recipe_id)
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   post '/recipes/:id/upvote', to: 'up_down_votes#upvote', as: :upvote
   post '/recipes/:id/downvote', to: 'up_down_votes#downvote', as: :downvote
+  post '/recipes/:id/add_review', to:'reviews#add_review', as: :add_review
 end
