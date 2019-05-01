6b97cc94e13e90dff7b2ff3c7107e4ae30b62ced
Kibisis <jdonahue@colgate.edu>
2019-04-30 20:41:51 -0400

    Adding user view history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     #byebug
     @recipes = @user.recipes
+
+    #history implementation
+    history = @user.view_history.split(":")
+    @history = []
+    seen = []
+    history.each do |hist_id|
+      if seen.include? hist_id
+        seen << hist_id
+        @history << Recipes.find(hist_id)
+      end
+    end
   end
 end
