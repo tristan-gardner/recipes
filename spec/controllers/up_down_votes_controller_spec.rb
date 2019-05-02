2204660670f06a56fb29e0cb2b4e9d0c07889f82
Tristan <trgardner123@gmail.com>
2019-05-02 15:04:39 -0400

    set up test environment

--- spec/controllers/up_down_votes_controller_spec.rb
+++ spec/controllers/up_down_votes_controller_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 require 'simplecov'
 
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+end
+
 RSpec.describe UpDownVotesController, type: :controller do
 
 end
